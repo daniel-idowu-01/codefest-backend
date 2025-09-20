788df579958470086d398b2ef4fccacc53a2c4a1
daniel-idowu-01 <danielidowu414@gmail.com>
2025-09-20 14:31:10 +0100

    feat: visits module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { VisitsModule } from './visits/visits.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule.register({
+      timeout: 10000,
+      maxRedirects: 5,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'maternal_health.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: process.env.NODE_ENV !== 'production',
+    }),
+    UsersModule, AuthModule, VisitsModule],
   controllers: [AppController],
   providers: [AppService],
 })
